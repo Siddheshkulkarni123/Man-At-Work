5bd0c8aac40835c8e1303f827d04958f1ae0cb4f
Siddhesh_Kulkarni <101642601+Siddheshkulkarni123@users.noreply.github.com>
2024-08-03 19:04:00 +0530

    Add files via upload

---
+++ page.js
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h2>Man At Work</h2>
+      {/* <button className="bg-red-600"></button> */}
+      {/* <Button className="bg-red-600">Submit</Button> */}
+    </div>
+  );
+}
